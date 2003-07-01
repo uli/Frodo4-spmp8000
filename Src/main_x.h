a6af5c103fa2897f01f9990c440e38f0555fa170
cebix <cebix>
2003-07-01 17:22:19 +0000

    imported files

---
+++ Src/main_x.h
@@ -0,0 +1,110 @@
+/*
+ *  main_x.h - Main program, Unix specific stuff
+ *
+ *  Frodo (C) 1994-1997,2002 Christian Bauer
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "Version.h"
+
+
+extern int init_graphics(void);
+
+
+// Global variables
+char Frodo::prefs_path[256] = "";
+
+
+/*
+ *  Create application object and start it
+ */
+
+int main(int argc, char **argv)
+{
+	Frodo *the_app;
+
+	timeval tv;
+	gettimeofday(&tv, NULL);
+	srand(tv.tv_usec);
+
+	printf("%s by Christian Bauer\n", VERSION_STRING);
+	if (!init_graphics())
+		return 0;
+	fflush(stdout);
+
+	the_app = new Frodo();
+	the_app->ArgvReceived(argc, argv);
+	the_app->ReadyToRun();
+	delete the_app;
+
+	return 0;
+}
+
+
+/*
+ *  Constructor: Initialize member variables
+ */
+
+Frodo::Frodo()
+{
+	TheC64 = NULL;
+}
+
+
+/*
+ *  Process command line arguments
+ */
+
+void Frodo::ArgvReceived(int argc, char **argv)
+{
+	if (argc == 2)
+		strncpy(prefs_path, argv[1], 255);
+}
+
+
+/*
+ *  Arguments processed, run emulation
+ */
+
+void Frodo::ReadyToRun(void)
+{
+	getcwd(AppDirPath, 256);
+
+	// Load preferences
+	if (!prefs_path[0]) {
+		char *home = getenv("HOME");
+		if (home != NULL && strlen(home) < 240) {
+			strncpy(prefs_path, home, 200);
+			strcat(prefs_path, "/");
+		}
+		strcat(prefs_path, ".frodorc");
+	}
+	ThePrefs.Load(prefs_path);
+
+	// Create and start C64
+	TheC64 = new C64;
+	if (load_rom_files())
+		TheC64->Run();
+	delete TheC64;
+}
+
+
+Prefs *Frodo::reload_prefs(void)
+{
+	static Prefs newprefs;
+	newprefs.Load(prefs_path);
+	return &newprefs;
+}
